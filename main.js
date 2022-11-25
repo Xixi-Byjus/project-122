a7993806b796a9c639705183a3b01d2a14de551a
Xixi-Byjus <107397970+Xixi-Byjus@users.noreply.github.com>
2022-11-25 18:45:19 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ screen_width = window.innerWidth;
+ screen_height = window.innerHeight;
+
+ canvas = createCanvas(screen_width, screen_height-150);
+ canvas.position(0,150)
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+var to_number="";
+
+var apple="";
+
+var speak_data="";
+
+function preload(){
+  apple = loadImage("apple.png");
+}
+
